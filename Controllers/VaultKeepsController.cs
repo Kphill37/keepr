ee5a2b071d7778a4deb8c954227ed9b9d96a6339
Kelly Phillips <fooxiefloof@gmail.com>
2019-07-22 10:43:56 -0600

    commit

---
+++ Controllers/VaultKeepsController.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using keepr.Models;
+using keepr.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keepr.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class VaultKeepsController : ControllerBase
+  {
+    private readonly VaultKeepRepository _repo;
+
+
+    public VaultKeepsController(VaultKeepRepository repo)
+    {
+      _repo = repo;
+    }
+
+
+    // GET api/vaults
+    [Authorize]
+    [HttpGet("{id}")]
+    public ActionResult<IEnumerable<Keep>> Get()
+    {
+      try
+      {
+        var userid = HttpContext.User.FindFirstValue("Id");
+        return Ok(_repo.GetALL(userid));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // GET api/vaults/5
+    // [Authorize]
+    // [HttpGet("{id}")]
+    // public ActionResult<Vault> Get(int id)
+    // {
+    //   try
+    //   {
+    //     var userid = HttpContext.User.FindFirstValue("Id");
+    //     return Ok(_repo.GetById(id, userid));
+    //   }
+    //   catch (Exception e)
+    //   {
+    //     return BadRequest(e);
+    //   }
+    // }
+
+    // GET api/keeps/user
+    // [Authorize]
+    // [HttpGet]
+    // public ActionResult<IEnumerable<Vault>> GetVaultsFromCurrentUser()
+    // {
+    //   try
+    //   {
+    //     var id = HttpContext.User.FindFirstValue("Id");
+
+    //     return Ok(_repo.GetVaultsFromCurrentUser(id));
+    //   }
+    //   catch (Exception e)
+    //   {
+    //     return BadRequest(e);
+    //   }
+    // }
+
+    // POST api/keeps
+    [Authorize]
+    [HttpPost]
+    public ActionResult<VaultKeep> Post([FromBody] VaultKeep value)
+    {
+      try
+      {
+        var id = HttpContext.User.FindFirstValue("Id");
+        value.userId = id;
+        return Ok(_repo.Create(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // PUT api/keeps/5
+    [Authorize]
+    [HttpPut("{id}")]
+    public ActionResult<Keep> Put(int id, [FromBody] Keep value)
+    {
+      try
+      {
+        value.id = id;
+        return Ok(_repo.Update(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // DELETE api/keeps/5
+    [HttpDelete("{id}")]
+    public ActionResult<String> Delete(int id)
+    {
+      try
+      {
+        return Ok(_repo.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+  }
+}
