3cfbc5d1269a0fcfa62898dd6303ee8c949f214f
Kphill37 <fooxiefloof@gmail.com>
2019-07-21 20:06:00 -0600

    commit

---
+++ Controllers/VaultsController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using keepr.Models;
+using keepr.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keepr.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class VaultsController : ControllerBase
+  {
+    private readonly VaultRepository _repo;
+
+
+    public VaultsController(VaultRepository repo)
+    {
+      _repo = repo;
+    }
+
+
+    // GET api/vaults
+    [Authorize]
+    [HttpGet]
+    public ActionResult<IEnumerable<Vault>> Get()
+    {
+      try
+      {
+        var userid = HttpContext.User.FindFirstValue("Id");
+        return Ok(_repo.GetALL(userid));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // GET api/vaults/5
+    [Authorize]
+    [HttpGet("{id}")]
+    public ActionResult<Vault> Get(int id)
+    {
+      try
+      {
+        return Ok(_repo.GetById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // GET api/keeps/user
+    // [Authorize]
+    // [HttpGet]
+    // public ActionResult<IEnumerable<Vault>> GetVaultsFromCurrentUser()
+    // {
+    //   try
+    //   {
+    //     var id = HttpContext.User.FindFirstValue("Id");
+
+    //     return Ok(_repo.GetVaultsFromCurrentUser(id));
+    //   }
+    //   catch (Exception e)
+    //   {
+    //     return BadRequest(e);
+    //   }
+    // }
+
+    // POST api/keeps
+    [Authorize]
+    [HttpPost]
+    public ActionResult<Vault> Post([FromBody] Vault value)
+    {
+      try
+      {
+        var id = HttpContext.User.FindFirstValue("Id");
+        value.userid = id;
+        return Ok(_repo.Create(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // PUT api/keeps/5
+    [Authorize]
+    [HttpPut("{id}")]
+    public ActionResult<Keep> Put(int id, [FromBody] Keep value)
+    {
+      try
+      {
+        value.id = id;
+        return Ok(_repo.Update(value));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+
+    // DELETE api/keeps/5
+    [HttpDelete("{id}")]
+    public ActionResult<String> Delete(int id)
+    {
+      try
+      {
+        return Ok(_repo.Delete(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e);
+      }
+    }
+  }
+}
---
+++ Repositories/VaultRepository.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using keepr.Models;
+
+namespace keepr.Repositories
+{
+  public class VaultRepository
+  {
+    private readonly IDbConnection _db;
+    public VaultRepository(IDbConnection db)
+    {
+      _db = db;
+    }
+
+    public IEnumerable<Vault> GetALL(string userid)
+    {
+      string query = "SELECT * FROM vaults where userId = @userid";
+      return _db.Query<Vault>(query, new { userid });
+    }
+
+    public Vault GetById(int id)
+    {
+      string query = @"SELECT * FROM vaults WHERE id = @id";
+      Vault vault = _db.QueryFirstOrDefault<Vault>(query, new { id });
+      if (vault == null) throw new Exception("Invalid Id");
+      return vault;
+    }
+
+    // public Keep GetById(int id)
+    // {
+    //   string query = @"SELECT * FROM keeps WHERE id = @id";
+    //   Keep keep = _db.QueryFirstOrDefault<Keep>(query, new { id });
+    //   if (keep == null) throw new Exception("Invalid Id");
+    //   return keep;
+    // }
+
+    // public IEnumerable<Vault> GetVaultsFromCurrentUser(string id)
+    // {
+    //   string query = @"SELECT * FROM vaults WHERE userid = @id";
+    //   return _db.Query<Vault>(query, new { id });
+    // }
+    public Vault Create(Vault value) //NOTE, variable names may have to be capitalized in the Keep.cs model
+    {
+      string query = @"
+            INSERT INTO vaults (name, description, userId)
+                    VALUES (@name, @description, @userid);
+                    SELECT LAST_INSERT_ID();
+            ";
+      int id = _db.ExecuteScalar<int>(query, value);
+      value.id = id;
+      return value;
+    }
+
+
+    public Keep Update(Keep value)
+    {
+      string query = @"
+            UPDATE keeps
+            SET
+                name = @name,
+                description = @description,
+                img = @img,
+                isPrivate = @isPrivate
+            WHERE id = @id;            
+            SELECT * FROM keeps WHERE id = @id";
+
+      return _db.QueryFirstOrDefault<Keep>(query, value);
+    }
+
+    public string Delete(int id)
+    {
+      string query = "DELETE FROM vaults WHERE id = @id";
+      int changedRows = _db.Execute(query, new { id });
+      if (changedRows < 1) throw new Exception("Invalid Id");
+      return "Successfully deleted Vault";
+
+    }
+  }
+}
